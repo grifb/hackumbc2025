fd917ce461f82fe28263cae6404e894fe4526cc2
MikeF310 <99669901+MikeF310@users.noreply.github.com>
2025-09-27 19:20:27 -0400

    Update Player.cs

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -12,7 +12,7 @@ public partial class Player : CharacterBody3D
 	[Export(PropertyHint.Range, "0,180,radians_as_degrees")] private float TiltAboveMax = Mathf.DegToRad(75f);
 	[Export(PropertyHint.Range, "0,180,radians_as_degrees")] private float TiltBelowMax = Mathf.DegToRad(50);
 	[Export] private float MoveSpeed = 10f;
-	[Export] private float SprintMultiplier = 1.15f;
+	[Export] private float SprintSpeed = 60f;
 	[Export] private float JumpForce = 50f;
 	[Export] private float Mass = 50f;
 	[Export] private float MouseSensitivity = 0.01f;
@@ -52,7 +52,7 @@ public partial class Player : CharacterBody3D
 
 		bool isSprinting = Input.IsActionPressed("Sprint");
 
-		MoveSpeed = isSprinting ? MoveSpeed * SprintMultiplier : MoveSpeed;
+		MoveSpeed = isSprinting ? SprintSpeed : 10f;
 
 		if (IsOnFloor() == false)
 		{
