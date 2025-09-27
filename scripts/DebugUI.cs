a23c0d57656cf934afb4c57dcda1254a044786ff
C4 Flame <jacquicannady@gmail.com>
2025-09-27 16:46:13 -0400

    jump update

--- scripts/DebugUI.cs
+++ scripts/DebugUI.cs
@@ -4,15 +4,18 @@ namespace Game;
 
 public partial class DebugUI : CanvasLayer
 {
-    private Label fps;
+	private Label fps;
+	private Label velocity;
 
-    public override void _Ready()
-    {
-        fps = GetNode<Label>("FPS");
-    }
+	public override void _Ready()
+	{
+		fps = GetNode<Label>("FPS");
+		velocity = GetNode<Label>("Velocity");
+	}
 
-    public override void _Process(double delta)
-    {
-        fps.Text = $"FPS: {Engine.GetFramesPerSecond()}";
-    }
+	public override void _Process(double delta)
+	{
+		fps.Text = $"FPS: {Engine.GetFramesPerSecond()}";
+		velocity.Text = GetNode<CharacterBody3D>("/root/Node3D/Player").Velocity.ToString();
+	}
 }
