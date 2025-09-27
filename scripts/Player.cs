a4eb5209e8748f611e625730b48d6357cd0ea117
MikeF310 <99669901+MikeF310@users.noreply.github.com>
2025-09-27 18:27:12 -0400

    Update Player.cs

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,12 +6,24 @@ public partial class Player : CharacterBody3D
 {
 	private Node3D cameraPivot;
 	private Camera3D camera;
+<<<<<<< Updated upstream
 	private SpringArm3D springArm;
+=======
+
+	private SpringArm3D springArm;
+
+>>>>>>> Stashed changes
 	[Export] private CanvasLayer pauseMenu;
 	private readonly float Gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity");
 	[Export(PropertyHint.Range, "0,180,radians_as_degrees")] private float TiltAboveMax = Mathf.DegToRad(75f);
 	[Export(PropertyHint.Range, "0,180,radians_as_degrees")] private float TiltBelowMax = Mathf.DegToRad(50);
 	[Export] private float MoveSpeed = 10f;
+<<<<<<< Updated upstream
+=======
+
+	[Export] private float SprintMultiplier = 1.15f;
+
+>>>>>>> Stashed changes
 	[Export] private float SprintSpeed = 60f;
 	[Export] private float JumpForce = 50f;
 	[Export] private float Mass = 50f;
@@ -20,6 +32,40 @@ public partial class Player : CharacterBody3D
 	public override void _Ready()
 	{
 		cameraPivot = GetNode<Node3D>("CameraPivot");
+<<<<<<< Updated upstream
+=======
+		camera = cameraPivot.GetNode<Camera3D>("SpringArm3D/Camera3D");
+
+		if (camera.Current)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+	}
+
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var direction = new Vector3(Input.GetAxis("Left", "Right"), 0, Input.GetAxis("Up", "Down")).Rotated(Vector3.Up, cameraPivot.Rotation.Y);
+		var velocity = direction * MoveSpeed;
+
+		bool isSprinting = Input.IsActionPressed("Sprint");
+
+		MoveSpeed = isSprinting ? MoveSpeed * SprintMultiplier : MoveSpeed;
+
+		if (IsOnFloor() == false)
+		{
+			velocity.Y = Velocity.Y - Mass * Gravity * (float)delta;
+		}
+		else if (Input.IsActionJustPressed("Jump"))
+		{
+			velocity.Y = JumpForce;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+
+>>>>>>> Stashed changes
 		springArm = cameraPivot.GetNode<SpringArm3D>("SpringArm3D");
 		camera = springArm.GetNode<Camera3D>("Camera3D");
 
@@ -45,6 +91,7 @@ public partial class Player : CharacterBody3D
 					);
 		}
 	}
+<<<<<<< Updated upstream
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -74,4 +121,6 @@ public partial class Player : CharacterBody3D
 			pauseMenu.Visible = true;
 		}
 	}
+=======
+>>>>>>> Stashed changes
 }
