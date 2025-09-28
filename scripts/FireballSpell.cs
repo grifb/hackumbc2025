223bbba80c9c6db60984d1b2979a5c3c962701e1
C4 Flame <jacquicannady@gmail.com>
2025-09-28 01:36:52 -0400

    Fireball Head

--- scripts/FireballSpell.cs
+++ scripts/FireballSpell.cs
@@ -12,46 +12,46 @@ using System.Diagnostics;
 
 public partial class FireballSpell : RigidBody3D, ISpell
 {
-    public int manaCost = 30;
-    public int castTime = 1;
-    public int ManaCost()
-    {
-        return manaCost;
-    }
+	public int manaCost = 30;
+	public int castTime = 1;
+	public int ManaCost()
+	{
+		return manaCost;
+	}
 
-    public int CastTime()
-    {
-        return 1;
-    }
+	public int CastTime()
+	{
+		return 1;
+	}
 
 
 
-    public void setRotation(Vector3 direction)
-    {
-        Rotation = direction;
-    }
+	public void setRotation(Vector3 direction)
+	{
+		Rotation = direction;
+	}
 
-    public void setPosition(Vector3 position)
-    {
-        GlobalPosition = position;
-    }
+	public void setPosition(Vector3 position)
+	{
+		GlobalPosition = position;
+	}
 
-    public void Launch(int speed)
-    {
-        LinearVelocity = new Vector3(0, 0, -speed) * GlobalTransform.Basis.Inverse();
+	public void Launch(int speed)
+	{
+		LinearVelocity = new Vector3(0, 0, -speed) * GlobalTransform.Basis.Inverse();
 
-    }
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
+	public override void _PhysicsProcess(double delta)
+	{
 
-        if (GetCollidingBodies().Count > 0)
-        {
-            Debug.Print("Collision Detected");
-            QueueFree();
-        }
-    }
-    
-    
+		if (GetCollidingBodies().Count > 0)
+		{
+			Debug.Print("Collision Detected");
+			QueueFree();
+		}
+	}
+	
+	
 	
 }
